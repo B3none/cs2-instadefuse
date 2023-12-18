0399d59b319df7021d105a96b69f9bf3ebf3a3a9
B3none <ablackham2000@gmail.com>
2023-12-18 06:49:30 +0000

    Continued implementing game events

--- InstadefusePlugin.cs
+++ InstadefusePlugin.cs
@@ -1,6 +1,7 @@
 ﻿using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Cvars;
 using Microsoft.Extensions.Logging;
 
 namespace InstadefusePlugin;
@@ -13,6 +14,9 @@ public class InstadefusePlugin : BasePlugin
     public override string ModuleAuthor => "B3none";
     public override string ModuleDescription => "Allows a CT to instantly defuse the bomb when all Ts are dead and nothing can prevent the defusal.";
 
+    // ConVars
+    private float _bombDuration;
+    
     private float c4PlantTime = 0.0f;
     private bool hasBeenDefused = false;
     private bool willMakeDefuse = false;
@@ -20,9 +24,11 @@ public class InstadefusePlugin : BasePlugin
     public override void Load(bool hotReload)
     {
         Console.WriteLine("Instadefuse plugin loaded!");
+        
+        _bombDuration = float.Parse(ConVar.Find("mp_c4timer")?.StringValue ?? string.Empty);
     }
     
-    [GameEventHandler(HookMode.Post)]
+    [GameEventHandler]
     public HookResult OnEventRoundStart(EventRoundStart @event, GameEventInfo info)
     {
         Logger.LogInformation("Round has started with Timelimit: {Timelimit}", @event.Timelimit);
@@ -31,5 +37,42 @@ public class InstadefusePlugin : BasePlugin
         willMakeDefuse = false;
         
         return HookResult.Continue;
+    }
+    
+    [GameEventHandler]
+    public HookResult OnPlayerDeath(EventPlayerBlind @event, GameEventInfo info)
+    {
+        AttemptInstadefuse();
+
+        return HookResult.Continue;
+    }
+
+    private void AttemptInstadefuse()
+    {
+        Logger.LogInformation("Instadefuse attempted.");
+        
+        if (_bombDuration == 0.0)
+        {
+            Logger.LogInformation("Bomb duration ConVar not set.");
+            return;
+        }
+
+        int defuser = GetDefuser();
+        
+        if (defuser < 1)
+        {
+            Logger.LogInformation("Defuser not found.");
+            return;
+        }
+        
+        var victim = @event.Userid;
+        var attacker = @event.Attacker;
+    }
+
+    private int GetDefuser()
+    {
+        
+        
+        return 0;
     }
 }
